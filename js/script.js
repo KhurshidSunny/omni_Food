3596119f628794f0985d19b09bd7750dabded38f
Khurshid Khan <khurshidsunny45@gmail.com>
2023-06-03 11:27:56 -0700

    Sticky Nav Bar is Added

--- js/script.js
+++ js/script.js
@@ -33,6 +33,31 @@ allLinks.forEach(function (link) {
   });
 });
 
+//////////////////////////////////////////////
+// Stick navBar
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
